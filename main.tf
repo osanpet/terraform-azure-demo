8879ad413700f6986da5b3b68c77619f8ee1b6d3
onuma <onuma17673@gmail.com>
2025-06-12 11:58:04 -0500

    add change

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 provider "azurerm" {
   features {}
+   subscription_id = "9d596013-053c-4763-9e3d-cd93773ea2ce"
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
 }
+#add change
